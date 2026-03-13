61b2ca5941cae340d445c941758202e8603a9b6f
Emre Meydan <ej.meydan@outlook.com>
2026-03-13 14:23:32 +0000

    Add solution for two-sum

--- solutions/two-sum.py
+++ solutions/two-sum.py
@@ -1,1 +1,9 @@
-undefined
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        #testing the extension :) 
+        
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
