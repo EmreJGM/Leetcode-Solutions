23dc87050c409e6fd5e68bc097d4e5615efe6f3b
Emre Meydan <ej.meydan@outlook.com>
2026-03-18 22:22:05 +0000

    lang: python3 | runtime: 3 ms | memory: 20.4 MB

--- solutions/two-sum.py
+++ solutions/two-sum.py
@@ -4,7 +4,8 @@ class Solution:
         seen = {}
 
         for i in range(len(nums)):
-            needed = target - nums[i]
+            
+            needed = target - nums[i]           
             if needed in seen:
                 return [seen[needed], i]
             else:
