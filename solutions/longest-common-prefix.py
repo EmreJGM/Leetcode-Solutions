d93ccce3cdd21e18581af12ab2769f0d4f42e336
Emre Meydan <ej.meydan@outlook.com>
2026-03-18 22:38:09 +0000

    lang: python3 | runtime: 0 ms | memory: 19.4 MB

--- solutions/longest-common-prefix.py
+++ solutions/longest-common-prefix.py
@@ -1,15 +1,15 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-
+        
         prefix = ""
 
-        for i in range(len(strs[0])):        
-            letter = strs[0][i]              
+        for i in range(len(strs[0])):
+            letter = strs[0][i]
 
-            for strr in strs[1:]:           
+            for strr in strs[1:]:
                 if i >= len(strr) or strr[i] != letter:
-                    return prefix            
-
-            prefix += letter                  
-
+                    return prefix
+            
+            prefix += letter
+        
         return prefix
